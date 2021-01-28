273ef9239cf47fb4c01385a01d6aaf23d2842b03
Leonardo Dias <leonardo_marquesdias@hotmail.com>
2021-01-28 16:36:59 +0000

    Second Ecercice

--- README.md
+++
@@ -1,2 +0,0 @@
-# three in one
-  3 exercises in a single page
--- script.js
+++ script.js
@@ -33,3 +33,53 @@ function carregar() {
   }
 }
 
+   //      ---        Exec-2        ---       // 
+
+
+function check() {
+   console.log(check)
+
+   const date = new Date()
+   const Year = date.getFullYear()
+   const formYear = document.getElementById('txtyear')
+   const resul = document.getElementById('res')
+   if (formYear.value.length == 0 || Number(formYear.value) > Year) {
+      window.alert('[ERRO] Verifique os dados e tente novamente!')
+   } else {
+     
+      const typeSex = document.getElementsByName('radsex')
+      const yAge = Year - Number(formYear.value)
+      var gender = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+         if (typeSex[0].checked) {
+            gender = 'Man'
+               if (yAge >=0 && yAge < 10) {
+                  img.setAttribute('src', './images/child-boy.png')
+               } else if (yAge < 21) {
+                  img.setAttribute('src', 'wphoto-yong-boy.png')
+               } else if (yAge < 50) {
+                  img.setAttribute('src', './images/man.png')
+               } else {
+                  img.setAttribute('src', './images/old-man.png')
+               }
+         } else if (typeSex[1].checked) {
+            gender = 'Woman'
+               if (yAge >=0 && yAge < 10) {
+                  img.setAttribute('src', './images/child-girl.png')
+               } else if (yAge < 21) {
+                  img.setAttribute('src', './images/girl.png')
+               } else if (yAge < 50) {
+                  img.setAttribute('src', './images/woman.png')
+               } else {
+                  img.setAttribute('src', './images/old-woman.png')
+               }
+         }
+         resul.innerHTML = `Detected ${gender} with ${yAge} Years Old`;
+         resul.appendChild(img)
+   }
+   
+
+}
+
+
